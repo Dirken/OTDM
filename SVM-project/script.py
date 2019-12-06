7ed1b05e84cf342021f3627d665472e2a0928f13
RMeyerhofer <ricard.meyerhofer@est.fib.upc.edu>
2019-12-06 12:58:30 +0100

    should be all

--- SVM-project/script.py
+++ SVM-project/script.py
@@ -14,21 +14,21 @@ def main():
 		print("nu= " + str(nu))
 		f.write(str(nu))
 		f.close()
-		os.system("ampl_mswin64\\ampl dual.run")
+		os.system("ampl_mswin64\\ampl primal.run")
 		
-		f = open("output/dual/acc_train.txt", "r") 
+		f = open("output/primal/acc_train.txt", "r") 
 		acc_train = f.read()
 		acc_trainValue = acc_train.split(" = ")[1].split("\n")[0]
 		print("Train accuracy= " + str(acc_trainValue))
 		f.close()
 
-		f = open("output/dual/acc_test.txt", "r") 
+		f = open("output/primal/acc_test.txt", "r") 
 		acc_test = f.read()
 		acc_testValue = acc_test.split(" = ")[1].split("\n")[0]
 		print("Test accuracy= " + str(acc_testValue))
 		f.close()
 
-		#os.system("dual.run")
+		#os.system("primal.run")
 
 
 
--- SVM-project/visualization.R
+++ SVM-project/visualization.R
@@ -1,24 +1,14 @@
 library(ggplot2)
 library(stringr)
 
-data <- read.csv("data/train100-seed54321.dat", sep = " ", header = F)
-#data$V5 <- str_detect(data$V5, "*")
-#data$V5 <- gsub('*', '', data$V5)
-data$V6 <- ifelse(data$V1 + data$V2 + data$V3 + data$V4 >= 2, 1, -1)
-data$V7 <- NULL
+results$nu <- c(0.03125, 0.0625, 0.125, 0.25, 0.5, 1 , 2, 4, 8, 16, 32, 64, 128, 256)
+results$train_acc <- c(0.53, 0.61, 0.82, 0.92, 0.94, 0.92, 0.94, 0.94, 0.93, 0.92, 0.92, 0.93, 0.93, 0.93)
+results$test_acc <- c(0.51, 0.585, 0.785, 0.875, 0.905, 0.915, 0.91, 0.93, 0.9, 0.905, 0.91, 0.91, 0.91, 0.91)
 
+results$nu[results$train_acc == max(results$train_acc)]
 
-sFile <- file("output/primal/s.txt", open = 'r')
-wFile <- file("output/primal/w.txt", open = 'r')
-gammaFile <- file("output/primal/gamma.txt", open = 'r')
-lambdaFile <- file("output/dual/lambda.txt", open = 'r')
+plot(log(nu), train_acc, type = 'b')
 
-s <- readLines(sFile)
-w <- readLines(wFile)
-gamma <- readLines(gammaFile)
-lambda <- readLines(lambdaFile)
+plot(log(nu), test_acc, type = 'b')
 
-#\d+\s*\d.\d*(e-\d\d|)
-
-a <- str_extract_all("1 1.40163e-10    26 1.35625        51 1.40229e-10    76 1.40162e-10 77 1", "\\d+\\s*\\d.\\d*(e-\\d\\d|)")
-a
+results$nu[results$test_acc == max(results$test_acc)]
